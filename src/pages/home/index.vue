263265a84e4db9be9d5110dae82b4fb2776eb225
Vine <chenjinbode@yeah.net>
2023-11-15 13:58:35 +0800

    首页基本逻辑完成

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -49,7 +49,7 @@
     icon="plus"
     magnetic="x" v-model:offset="offset" @click="addTodo" />
     <van-notify></van-notify>
-    <van-pull-refresh v-model="refreshing" @refresh="onRefresh"  v-if="list.length">
+    <van-pull-refresh v-model="refreshing" disabled  @refresh="onRefresh"  v-if="list.length">
       <van-list
         class="list"
         v-model:loading="loading"
@@ -65,83 +65,27 @@
             </template>
           </van-cell>
           <template #right>
-            <van-button class="btn" square type="primary" text="编辑" @click="handleTodo('update', item.id)" />
-            <van-button class="btn" square type="danger" text="删除" @click="handleTodo('delete', item.id)" />
+            <van-button class="btn" square type="primary" text="编辑" @click="handleTodo('update', item)" />
+            <van-button class="btn" square type="danger" text="删除" @click="handleTodo('delete', item)" />
           </template>
         </van-swipe-cell>
       </van-list>
     </van-pull-refresh>
     <van-empty description="暂无数据"  v-else/>
-
-    <van-dialog v-model:show="todoShow" title="编辑" :showConfirmButton="false" :close-on-click-overlay="true">
-      <van-form @submit="onSubmit"  label-width="60" colon validate-trigger="['onSubmit', 'onChange']">
-        <van-cell-group inset>
-          <van-field
-            required
-            v-model="todoForm.title"
-            name="title"
-            label="标题"
-            placeholder="请填写标题"
-            :rules="[{ required: true, message: '请填写标题!' }]"
-          />
-          <van-field
-            required
-            v-model="todoForm.level"
-            is-link
-            readonly
-            name="picker"
-            label="优先级"
-            placeholder="点击选择优先级"
-            :rules="[{ required: true, message: '请选择优先级!' }]"
-            @click="showLevelPicker = true"
-          />
-          <van-field
-            required
-            v-model="todoForm.content"
-            name="content"
-            label="内容"
-            type="textarea"
-            placeholder="请填写内容"
-            :rules="[{ required: true, message: '请填写内容!' }]"
-          />
-          <van-field
-            v-model="todoForm.comment"
-            name="comment"
-            label="备注"
-            placeholder="请填写备注"
-            :rules="[{ required: true, message: '请填写备注!' }]"
-          />
-        </van-cell-group>
-        <div style="margin: 16px;">
-          <van-button round block type="primary" native-type="submit">
-            登录
-          </van-button>
-        </div>
-      </van-form>
-    </van-dialog>
-    <van-popup v-model:show="showLevelPicker" position="bottom">
-      <van-picker
-        :columns="columns"
-        @confirm="onConfirm"
-        :columns-field-names="columnsFieldNames"
-        @cancel="showLevelPicker = false"
-      />
-    </van-popup>
   </div>
+  <todo-item :show="todoShow" :data="todoForm" :is-add="isAdd" @close="onCloseTodoItem"/>
+  <van-toast></van-toast>
 </template>
 
 <script setup lang="ts">
 import Api from '@/api';
 import { ref, onMounted, computed, shallowRef, watch, reactive } from 'vue';
+import { showNotify, showConfirmDialog, showToast } from 'vant';
+import todoItem from './todoItem.vue';
 // @ts-ignore
 import CryptoJS from 'crypto-js';
 import { secretPwKey } from '@/config/secret'; 
 import { levels } from '@/config';
-import { showNotify, showConfirmDialog, showToast } from 'vant';
-const columns = levels
-const columnsFieldNames = {
-  text: 'label'
-}
 const token = ref(localStorage.getItem('token'))
 const show = ref(false)
 const username = ref('');
@@ -163,7 +107,6 @@ const onSubmit = async (values: any) => {
     localStorage.setItem('userid', data.id)
     localStorage.setItem('userinfo', JSON.stringify(data))
   } catch (error: any) {
-    console.log(error, 'error')
     showNotify({ color: '#ad0000', background: '#ffe1e1', type: 'danger', message: error.message });
   }
 }
@@ -210,7 +153,6 @@ const getList = async () => {
 }
 const refreshing = ref(false)
 const onRefresh = () => {
-  console.log('onRefresh')
     // 清空列表数据
     finished.value = false;
     // 重新加载数据
@@ -223,7 +165,6 @@ const deleteItem = async () => {
   if (!token.value) return
   try {
     const { data } = await Api.deleteTodo({id: listId.value})
-    console.log(data, 'deleteItem')
     list.value = []
     pageNum.value = 1
     showToast('删除成功!')
@@ -233,21 +174,20 @@ const deleteItem = async () => {
   }
 }
 const todoShow = ref(false)
-const todoForm = reactive({
-  title: '',
-  content: '',
-  level: '',
-  startTime: null,
-  endTime: null,
-  comment: ''
-})
-const showLevelPicker = ref(false)
-const onConfirm = ({ selectedOptions }: { selectedOptions: any[]}) => {
-  console.log(selectedOptions, 'selectedOptions')
+const onCloseTodoItem = () => {
+  todoShow.value = false
+  todoForm.value = {
+    title: '',
+    content: '',
+    level: '',
+    commont: ''
+  }
+  list.value = []
+  pageNum.value = 1
+  getList()
 }
-const todoType = ref('')
+const todoForm = shallowRef()
 const beforeClose = ({ position }: { position: string }) => {
-  console.log(position, 'position')
   const isUpdate = todoType.value === 'update'
   switch (position) {
     case 'left':
@@ -256,7 +196,6 @@ const beforeClose = ({ position }: { position: string }) => {
       return true;
     case 'right':
       if (isUpdate) {
-        console.log('进入更新方法')
         return false
       }
       return new Promise((resolve) => {
@@ -272,17 +211,29 @@ const beforeClose = ({ position }: { position: string }) => {
       })
   }
 }
-const handleTodo = (type: string, id: string) => {
-  console.log(type, 'handleTodo', id)
-  listId.value = id
+const todoType = ref('')
+const isAdd = ref(true)
+const handleTodo = (type: string, item: any) => {
+  listId.value = item.id
   todoType.value = type
+  if (type === 'update') {
+    todoForm.value = item
+    todoShow.value = true
+    isAdd.value = false
+  }
 }
 const offset = ref({ x: 300, y: 500 })
 const addTodo = () => {
-  console.log('addTodo')
+  todoShow.value = true
+  isAdd.value = true
+  todoForm.value = {
+    title: '',
+    content: '',
+    level: '',
+    commont: ''
+  }
 }
 watch(() => userinfo.value?.username, (val) => {
-  console.log(val, 'val')
   if (val) {
     pageNum.value = 1
     getList()
--- vite.config.ts
+++ vite.config.ts
@@ -21,7 +21,7 @@ export default defineConfig(({ mode }) => {
       allowImportingTsExtensions: true,
     },
     server: {
-      host: 'localhost',
+      host: '0.0.0.0',
       port: 8080,
       cors: true,
       proxy: {
